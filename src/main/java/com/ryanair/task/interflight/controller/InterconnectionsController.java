2f5906bd32e9f2657fdc21a528ba904e70be6039
Gabriel Leite <gabrielholeite@gmail.com>
2023-08-28 20:54:24 +0100

    Start of the project and create InterconnectionsController.java

---
+++ src/main/java/com/ryanair/task/interflight/controller/InterconnectionsController.java
@@ -0,0 +1,29 @@
+package com.ryanair.task.interflight.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api")
+public class InterconnectionsController {
+
+    static Logger log = Logger.getLogger(InterconnectionsController.class.getName());
+
+    @GetMapping("/interconnections")
+    public void getInterconnections(@RequestParam String departure,
+                                    @RequestParam String arrival,
+                                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime departureDateTime,
+                                    @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime arrivalDateTime) {
+        log.info("Departure = " + departure +
+                ", Arrival = " + arrival +
+                ", Departure DATE_TIME = " + departureDateTime +
+                ", Arrival DATE_TIME = " + arrivalDateTime);
+    }
+
+}
---
+++ src/test/java/com/ryanair/task/interflight/InterFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanair.task.interflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterFlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
